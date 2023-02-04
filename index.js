e96c604ea8b6148b68a5b107067b942509d70814
Ghanishtkhurana <101393463+Ghanishtkhurana@users.noreply.github.com>
2023-02-05 00:37:02 +0530

    backend files

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const cors = require("cors")
+const userRouter = require("./routes/users.route")
+const productRouter = require("./routes/product.route")
+const cartRouter = require("./routes/cart.route")
+const app = express()
+const connect = require("./config/db")
+
+app.use(cors())
+app.use("/users",userRouter)
+app.use("/products",productRouter)
+app.use("/carts",cartRouter)
+
+app.get("/",(req,res)=>{
+    res.send("Home page")
+})
+
+
+app.listen(8080,async()=>{
+    await connect()
+    console.log('server is started on 8080')
+})
---
+++ routes/cart.route.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const auth = require("../middleware/auth.middleware");
+const Cart = require("../models/cart.model");
+const app = express.Router();
+const jwt = require("jsonwebtoken")
+
+app.use(express.json());
+app.use(auth);
+
+app.get("/", async (req, res) => {
+  try {
+    let token = req.headers.authorization
+    let decode = jwt.verify(token,"SECRET123")
+    console.log("decode=>",decode)
+    const cart = await Cart.find({userId : decode.id});
+    res.send(cart);
+  } catch (e) {
+    res.send(e.message);
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const cartItem = await Cart.create({ ...req.body });
+    res.send(cartItem);
+  } catch (e) {
+    res.send(e.message);
+  }
+});
+
+app.patch("/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
+    const cartItem = await Cart.findByIdAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true }
+    );
+    res.send(cartItem);
+  } catch (e) {
+    res.send(e.message);
+  }
+});
+
+app.delete("/:id",async(req,res)=>{
+  try{
+    const {id} = req.params
+    const cartItem = await Cart.findByIdAndDelete({_id : id})
+    res.send("Item is deleted from your cart")
+  }
+  catch(e){
+    res.send(e.message)
+  }
+})
+
+module.exports = app;
